3e9ba984cad1a507adc1213587bd506ba08dd561
Pavel Halkovsky <pavelhalkovsky@gmail.com>
2026-04-17 22:35:44 +0300

    Small bug fixings.

--- src/BigDecimal.js
+++ src/BigDecimal.js
@@ -4,6 +4,9 @@ class BigDecimal {
     #decimalPrecision = 1000;
     
     constructor(integer, decimal, decimalPrecision = 1000) {
+        if (typeof decimalPrecision !== 'number') throw new TypeError("Precision must be an integer number.");
+        if (Math.floor(decimalPrecision) !== decimalPrecision) throw new RangeError("Precision must be an integer number.");
+        
         this.#integer = validateAndNormalize(integer);
         this.#decimal = validateAndNormalize(decimal);
         this.#decimalPrecision = decimalPrecision;
@@ -56,8 +59,8 @@ class BigDecimal {
         return this.#integer.toString() + '.' + this.#decimal.toString();
     }
     
-    #precise(n) {
-        this.#decimal.substring(0, this.#decimalPrecision);
+    #precise() {
+        this.#decimal = this.#decimal.substring(0, this.#decimalPrecision);
     }
     
     #validateAndNormalize(n) {
@@ -74,7 +77,7 @@ class BigDecimal {
         n = n.split('.');
         if (n.length === 1) n.push('0');
         
-        return new BigDecimal(n[0], n[1]);
+        return new BigDecimal(n[0], n[1], this.#decimalPrecision);
     }
     
     [Symbol.toPrimitive]() {
@@ -84,7 +87,7 @@ class BigDecimal {
     [Symbol.toStringTag] = 'BigDecimal';
 }
 
-const a = new BigDecimal('9', '0001', 10);
+const a = new BigDecimal('9', '000123456789', 10);
 const b = new BigDecimal('9', '9999', 10);
 a.add(b).add(88).add(17.005).add('8785').add('0.00095');
 console.log(a.toString());
